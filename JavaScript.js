11b721719c7f94c4f7fb6122c01fed577dec106b
AndresTimaure21 <andrestimaured27@gmail.com>
2023-07-09 18:22:34 -0300

    listo para entregar

--- JavaScript.js
+++ JavaScript.js
@@ -1,49 +1,21 @@
- // Obtener referencia al botón
- const botonModoNocturno = document.getElementsByClassName('modoNocturno')[0];
+ // Obtén el botón de modo nocturno
+ const modoNocturnoBtn = document.querySelector('.modoNocturno');
 
- // Cambiar la posición del botón
-botonModoNocturno.style.position = "fixed";
-botonModoNocturno.style.top = "50%";
-botonModoNocturno.style.left = "80%";
-
-
-// Obtener referencia al elemento body
-const body = document.body;
-
-// Agregar evento de clic al botón
-botonModoNocturno.addEventListener('click', function() {
-  // Alternar la clase modo-nocturno en el elemento body
-  body.classList.toggle('modo-nocturno');
-});
-
-
-
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-
-
-  burger.addEventListener("click", () => {
-    console.log("Click en el menú hamburguesa"); 
-    nav.classList.toggle("nav-active");
-  });
-};
-
-navSlide();
-
-//console.log(burger); // Verificar si el elemento .burger está siendo seleccionado correctamente
-
-if (window.localStorage) {
-  // localStorage es compatible
-  // Realiza acciones utilizando localStorage
-} else {
-  // localStorage no es compatible
-  // Proporciona una funcionalidad alternativa o informa al usuario
-}
-
-if (navigator.userAgentData) {
-  const userAgentData = navigator.userAgentData;
-  // Accede a la información del agente de usuario utilizando userAgentData
-} else {
-  // Utiliza una técnica alternativa para obtener información del agente de usuario
-}
+ // Agrega un evento de clic al botón de modo nocturno
+ modoNocturnoBtn.addEventListener('click', function() {
+   // Agrega o remueve una clase para cambiar el estilo del modo nocturno
+   document.body.classList.toggle('modo-nocturno');
+ });
+ 
+ function toggleDescription(button) {
+   var cursosCard = button.parentNode;
+   var cardDescrip = cursosCard.querySelector('.card-descrip');
+ 
+   cursosCard.classList.toggle('expanded');
+ 
+   if (cursosCard.classList.contains('expanded')) {
+     button.textContent = 'VER MENOS';
+   } else {
+     button.textContent = 'VER MÁS';
+   }
+ }
